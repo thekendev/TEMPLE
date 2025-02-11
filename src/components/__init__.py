91d36cd7da59ad09dbf8d6492114a96ff492fa48
Justice Kenechukwu Kanayo <justicesmacboookair@Justices-MacBook-Air.local>
2025-02-11 05:42:18 +0000

    Logging and exception

---
+++ src/components/__init__.py
@@ -0,0 +1,7 @@
+'''
+This is mainly for the training purpose
+Here all the modules would be in the folder
+e.g Data ingestion which is just reading the data, 
+then maybed data validation would be in here,
+and data transformation
+'''
---
+++ src/pipeline/__init__.py
@@ -0,0 +1,4 @@
+'''
+this is mainly about training and predicting pipeline 
+and thiese files will communicate & trigger to the components
+'''
