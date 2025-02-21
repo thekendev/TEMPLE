8729a009ae4fb8160b83236029e68f4e1c257c68
Justice Kenechukwu Kanayo <justicesmacboookair@Justices-MacBook-Air.local>
2025-02-21 18:38:16 +0000

    [TMP-35]: Template file created [User Story: TMP-9]

---
+++ template.py
@@ -0,0 +1,54 @@
+"""
+This script automates the creation of the folder structure and template files 
+for the project, ensuring that the necessary directories and files are in place 
+for development.
+
+It generates the following:
+- folder_structure/
+- template.py
+- config/ directory with config files
+"""
+
+import os
+from pathlib import Path
+import logging
+
+# logging string
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = 'Temple'
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "templates/index.html" 
+]
+
+# files & folders logic
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
